a85d3c96a5f2d9db551cb600795810ec86748861
Will Locklier <will.locklier@gmail.com>
2020-10-21 15:36:17 -0400

    [starwars] Add html and JS file

---
+++ star-wars.js
@@ -0,0 +1,12 @@
+const starwars = {
+    image: "path to where image is located",
+    length: "1:00",
+    title: "Special Look | The Mandalorian | Disney+",
+    username: "Star Wars",
+    views: "1M",
+    age: "1 day",
+
+}
+
+console.log(starwars.title);
+console.log(starwars.username);
